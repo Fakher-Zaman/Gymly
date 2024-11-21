3cec9866b7701c06db3c2aed83b42ca3b9c78e7f
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-11-21 15:16:33 +0500

    The use of contextAPI for supplying Auth Service

---
+++ src/appwrite/AppwriteContext.tsx
@@ -0,0 +1,33 @@
+import { View, Text } from 'react-native';
+import React, { createContext, FC, PropsWithChildren, useState } from 'react';
+
+import Appwrite from './service';
+
+type AppContextType = {
+    appwrite: Appwrite;
+    isLoggedIn: boolean;
+    setIsLoggedIn: (value: boolean) => void;
+}
+
+export const AppwriteContext = createContext<AppContextType>({
+    appwrite: new Appwrite(),
+    isLoggedIn: false,
+    setIsLoggedIn: () => { }
+})
+
+export const AppwriteProvider: FC<PropsWithChildren> = ({ children }: any) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const defaultValue = {
+        appwrite: new Appwrite(),
+        isLoggedIn,
+        setIsLoggedIn,
+    }
+
+    return (
+        <AppwriteContext.Provider value={defaultValue}>
+            {children}
+        </AppwriteContext.Provider>
+    )
+}
+
+export default AppwriteContext;
